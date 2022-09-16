bf33df4436cbe6fd841f9f0d3a1fb4ddbde2139d
marimontod-kc <mariana.montoya@kinandcarta.com>
2022-09-16 16:15:55 -0500

    create custom clients

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
-
+ 
   onSearchSubmit = async (term) => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await unsplash.get("/search/photos", {
       params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296",
-      },
     });
     this.setState({ images: response.data.results });
   };
